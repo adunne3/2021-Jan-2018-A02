49937c991bcdd702e80406fdd1a5580eff5e801d
adunne3 <59737554+adunne3@users.noreply.github.com>
2021-01-18 21:34:53 -0700

    Create albums entiity

--- ChinookSolution/ChinookSystem/Entities/Album.cs
+++ ChinookSolution/ChinookSystem/Entities/Album.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+#region Additional Namespace
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
 namespace ChinookSystem.Entities
 {
-    class Album
+    [Table("Albums")]
+    internal class Album
     {
+        private string _ReleaseLabel;
+        [Key]
+        public int AlbumId { get; set; }
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(50, ErrorMessage = "Title is limited to 60 chars")]
+        public string Title { get; set; }
+        public int ArtistId { get; set; }
+        public int ReleaseYear { get; set; }
+        [StringLength(50, ErrorMessage = "Album release label is limited to 50 chars")]
+        public string ReleaseLabel 
+        {
+            get
+            {
+                return _ReleaseLabel;
+            }
+            set
+            {
+                _ReleaseLabel = string.IsNullOrEmpty(value) ? null : value;
+            }
+        }
     }
 }
