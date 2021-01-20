d4aec91d38a5ce13baf6f468d7106ae7305ef0a0
adunne3 <59737554+adunne3@users.noreply.github.com>
2021-01-19 17:28:22 -0700

    Created track entity

--- ChinookSolution/ChinookSystem/Entities/Track.cs
+++ ChinookSolution/ChinookSystem/Entities/Track.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+#region Additional Namespace
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
 namespace ChinookSystem.Entities
 {
-    class Track
+    [Table("Tracks")]
+    internal class Track
     {
+        private string _Composer;
+        [Key]
+        public int TrackId { get; set; }
+        [Required(ErrorMessage ="A track name is required")]
+        [StringLength(200, ErrorMessage = "Track name is limited to 200 characters"), MinLength(1)]
+        public string Name { get; set; }
+        public int? AlbumId { get; set; }
+        public int MediaTypeId { get; set; }
+        public int? GenreId { get; set; }
+        [StringLength(220, ErrorMessage = "Track composer is limited to 220 characters")]
+        public string Composer
+        {
+            get { return _Composer; }
+            set { _Composer = string.IsNullOrEmpty(value) ? null : value; }
+        }
+        public int Milliseconds { get; set; }
+        public int? Bytes { get; set; }
+        public decimal UnitPrice { get; set; }
+
+        //Nav properties child to parent (many to one)
+        public virtual Album Album { get; set; }
+        public virtual Genre Genre { get; set; }
+        public virtual MediaType MediaType { get; set; }
     }
 }
