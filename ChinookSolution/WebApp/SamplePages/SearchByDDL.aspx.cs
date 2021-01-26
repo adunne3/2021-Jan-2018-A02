1cbc5f427b7a4e40c724054d2980f19aa139084c
adunne3 <59737554+adunne3@users.noreply.github.com>
2021-01-26 09:40:52 -0700

    Created SelectCheckForException as an Example of Error Handling ODS

--- ChinookSolution/WebApp/SamplePages/SearchByDDL.aspx.cs
+++ ChinookSolution/WebApp/SamplePages/SearchByDDL.aspx.cs
@@ -22,6 +22,12 @@ namespace WebApp.SamplePages
 
             }
         }
+        #region Error Handling ODS
+        protected void SelectCheckForException(object sender, ObjectDataSourceStatusEventArgs e)
+        {
+            MessageUserControl.HandleDataBoundException(e);
+        }
+        #endregion
         protected void LoadArtistList()
         {
             ArtistController sysmgr = new ArtistController();
